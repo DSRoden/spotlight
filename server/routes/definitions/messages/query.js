11633ac6a98a45de847ed9d32bddcdae59939f9a
DSRoden <Daniel.S.Roden@gmail.com>
2014-12-28 12:44:13 -0500

    routes for photos and messages to show publically

--- server/routes/definitions/messages/query.js
+++ server/routes/definitions/messages/query.js
@@ -5,6 +5,7 @@ var Message = require('../../../models/message');
 module.exports = {
   description: 'Query Messages',
   tags:['messages'],
+  auth: false,
   handler: function(request, reply){
     Message.query(function(err, messages){
       //console.log('getting messages back in query', messages);
--- server/routes/definitions/photos/query.js
+++ server/routes/definitions/photos/query.js
@@ -5,6 +5,7 @@ var Photo = require('../../../models/photo');
 module.exports = {
   description: 'Query Photos',
   tags:['photos'],
+  auth: false,
   handler: function(request, reply){
     Photo.query(function(err, photos){
       console.log('getting messages back in query', photos);
