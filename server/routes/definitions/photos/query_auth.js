242877d910a415fa6678a30ac96e37467522e62e
DSRoden <Daniel.S.Roden@gmail.com>
2014-12-31 11:13:01 -0500

    following photos routes

--- server/routes/definitions/photos/query_auth.js
+++ server/routes/definitions/photos/query_auth.js
@@ -6,6 +6,7 @@ module.exports = {
   description: 'Query Photos',
   tags:['images'],
   handler: function(request, reply){
+    console.log('inside query_auth for photos');
     Photo.queryAuth(request.auth.credentials, function(err, photos){
       console.log('getting messages back in query', photos);
       reply(photos).code(err ? 400 : 200);
